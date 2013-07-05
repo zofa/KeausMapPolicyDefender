034f4d24d24d23349cff34e5c2fdc0ed1a260ce3
zofa <zafar@kraususa.com>
2013-07-05 11:59:39 -0400

    123

--- export_history.php
+++ export_history.php
@@ -1,9 +1,9 @@
 <html>
 <body>
  <?php
-include('db.php');
-
-$dbTable="";
+	include ('db.php');
+	
+	$dbTable = "";
 	$sql = "select catalog_product_flat_1.sku,
 catalog_product_flat_1.name as pname,
 website.name as wname, 
@@ -26,33 +26,31 @@ and
 crawl.id = 
 (select max(crawl.id) from crawl)
 order by sku asc";
-
-	$result = mysql_query($sql)	or die("Couldn't execute query:<br>".mysql_error().'<br>'.mysql_errno());
-
-	header('Content-Type: application/vnd.ms-excel');	//define header info for browser
-	header('Content-Disposition: attachment; filename='.$dbTable.'-'.date('Ymd'));
-	header('Pragma: no-cache');
-	header('Expires: 0');
-
+	
+	$result = mysql_query ( $sql ) or die ( "Couldn't execute query:<br>" . mysql_error () . '<br>' . mysql_errno () );
+	
+	header ( 'Content-Type: application/vnd.ms-excel' ); // define header info for browser
+	header ( 'Content-Disposition: attachment; filename=' . $dbTable . '-' . date ( 'Ymd' ) );
+	header ( 'Pragma: no-cache' );
+	header ( 'Expires: 0' );
+	
 	echo '<table><tr>';
-	for ($i = 0; $i < mysql_num_fields($result); $i++)	 // show column names as names of MySQL fields
-		echo '<th>'.mysql_field_name($result, $i).'</th>';
-	print('</tr>');
-
-	while($row = mysql_fetch_row($result))
-	{
-		//set_time_limit(60); // you can enable this if you have lot of data
+	for($i = 0; $i < mysql_num_fields ( $result ); $i ++) // show column names as names of MySQL fields
+		echo '<th>' . mysql_field_name ( $result, $i ) . '</th>';
+	print ('</tr>') ;
+	
+	while ( $row = mysql_fetch_row ( $result ) ) {
+		// set_time_limit(60); // you can enable this if you have lot of data
 		$output = '<tr>';
-		for($j=0; $j<mysql_num_fields($result); $j++)
-		{
-			if(!isset($row[$j]))
+		for($j = 0; $j < mysql_num_fields ( $result ); $j ++) {
+			if (! isset ( $row [$j] ))
 				$output .= '<td>&nbsp;</td>';
 			else
 				$output .= "<td>$row[$j]</td>";
 		}
-		print(trim($output))."</tr>\t\n";
+		print (trim ( $output )) . "</tr>\t\n";
 	}
-	echo('</table>');
-?>
+	echo ('</table>');
+	?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 
 <link href="css/templatemo_style.css" rel="stylesheet" type="text/css" />
 
-<script type="text/javascript" src="js/jquery-1-4-2.min.js"></script> 
- 
-<script type="text/javascript" src="js/jquery-ui.min.js"></script> 
-<script type="text/javascript" src="js/showhide.js"></script> 
-<script type="text/JavaScript" src="js/jquery.mousewheel.js"></script> 
+<script type="text/javascript" src="js/jquery-1-4-2.min.js"></script>
+
+<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/showhide.js"></script>
+<script type="text/JavaScript" src="js/jquery.mousewheel.js"></script>
 
 <link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
 
@@ -31,7 +31,7 @@ ddsmoothmenu.init({
 	contentsource: "markup" 
 })
 
-</script> 
+</script>
 
 <script src="js/tabs_old.js"></script>
 <script type="text/javascript">
@@ -52,7 +52,7 @@ ddsmoothmenu.init({
     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
   }
 </script>
- 
+
 
 
 <link href="css/TBLCSS.css" rel="stylesheet" type="text/css" />
@@ -62,60 +62,68 @@ ddsmoothmenu.init({
 
 <body id="home">
 
-<div id="templatemo_header_wrapper">
-	<div ><a href="http://www.kraususa.com/"><img src="images/images.jpg" width="186" height="84" /> </a>
-    <img align="top" src="images/head1.PNG" /></div>
-     <div id="templatemo_menu" class="ddsmoothmenu">
-             <br style="clear: left" />
-    </div> 
-    <div class="cleaner" align="center"></div>
-</div>	
+	<div id="templatemo_header_wrapper">
+		<div>
+			<a href="http://www.kraususa.com/"><img src="images/images.jpg"
+				width="186" height="84" /> </a> <img align="top"
+				src="images/head1.PNG" />
+		</div>
+		<div id="templatemo_menu" class="ddsmoothmenu">
+			<br style="clear: left" />
+		</div>
+		<div class="cleaner" align="center"></div>
+	</div>
 
-<div id="wrapper" align="center">
-  
-  <div id="tabContainer" align="center">
-    <div id="tabs" align="center">
-      <ul>
-        <li id="tabHeader_1">Recent Violations</li>
-        <li id="tabHeader_2">Violation By Product</li>
-        <li id="tabHeader_3">Violation By Seller</li>
-        <li id="tabHeader_4">Violation History</li>
-      </ul>
-    </div>
-    <div id="tabscontent" align="center">
-    
-      <div class="tabpage" id="tabpage_1">
-    
-   <iframe src="recent.php" style=" width:940px; hight:100%; overflow:visible; frameborder=0;"  onload='javascript:resizeIframe(this);' ></iframe>
-     
-      </div>
-      
-      <div class="tabpage" id="tabpage_2">
-      
-        <iframe src="product.php" style=" width:940px; hight:100%;  frameborder=0; "onload='javascript:resizeIframe(this);'>       </iframe>
-      </div>
-      
-      <div class="tabpage" id="tabpage_3">
-     
-          <iframe src="vendor.php" style=" width:940px; hight:100%; overflow:hidden; frameborder=0; " onload='javascript:resizeIframe(this);'>      </iframe>
-            </div>
-          <div class="tabpage" id="tabpage_4">
-          
-         <iframe src="History.php" style="height:100%; width:940px; overflow:hidden; frameborder=0;"  onload='javascript:resizeIframe(this);'>         </iframe>
-            </div>    
-        
-  </div>
+	<div id="wrapper" align="center">
 
-<div class="cleaner"></div>
+		<div id="tabContainer" align="center">
+			<div id="tabs" align="center">
+				<ul>
+					<li id="tabHeader_1">Recent Violations</li>
+					<li id="tabHeader_2">Violation By Product</li>
+					<li id="tabHeader_3">Violation By Seller</li>
+					<li id="tabHeader_4">Violation History</li>
+				</ul>
+			</div>
+			<div id="tabscontent" align="center">
 
-    	<div class="cleaner"></div>
-    </div> 
-</div> 
+				<div class="tabpage" id="tabpage_1">
 
-<div id="templatemo_footer_wrapper">
-    <div id="templatemo_footer">
-    	Copyright © Kuaus USA 2013
-    </div> 
-</div> 
+					<iframe src="recent.php"
+						style="width: 940px; hight: 100%; overflow: visible;"
+						onload='javascript:resizeIframe(this);'></iframe>
+
+				</div>
+
+				<div class="tabpage" id="tabpage_2">
+
+					<iframe src="product.php" style="width: 940px; hight: 100%;"
+						onload='javascript:resizeIframe(this);'> </iframe>
+				</div>
+
+				<div class="tabpage" id="tabpage_3">
+
+					<iframe src="vendor.php"
+						style="width: 940px; hight: 100%; overflow: hidden;"
+						onload='javascript:resizeIframe(this);'> </iframe>
+				</div>
+				<div class="tabpage" id="tabpage_4">
+
+					<iframe src="History.php"
+						style="height: 100%; width: 940px; overflow: hidden;"
+						onload='javascript:resizeIframe(this);'> </iframe>
+				</div>
+
+			</div>
+
+			<div class="cleaner"></div>
+
+			<div class="cleaner"></div>
+		</div>
+	</div>
+
+	<div id="templatemo_footer_wrapper">
+		<div id="templatemo_footer">Copyright © Kuaus USA 2013</div>
+	</div>
 </body>
 </html>
